1464af057a22cd94b1f83129ed15b99203d26176
bgmastery <60539405+bgmastery@users.noreply.github.com>
2021-02-07 15:43:25 +0200

    Add project files.

---
+++ Informational system Lib/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Informational_system_Lib.Models
+{
+    public class Book
+    {
+        [Key]
+        public int IdBook { get; set; }
+        public string Zaglavie { get; set; }
+        public string Avtor { get; set; }
+        public string Janr { get; set; }
+        public int Kolichestvo { get; set; }
+    }
+}
